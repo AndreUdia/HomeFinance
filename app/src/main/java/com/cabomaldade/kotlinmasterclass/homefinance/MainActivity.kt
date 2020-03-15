f74b4bbaefa87931be206e52f4cdf3babfd71ff2
Cabomaldade <soutom@yahoo.com.br>
2020-03-15 10:02:15 -0300

    botoes consistentes, implementado radiobutton group e salvar transacoes

--- app/src/main/java/com/cabomaldade/kotlinmasterclass/homefinance/MainActivity.kt
+++ app/src/main/java/com/cabomaldade/kotlinmasterclass/homefinance/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.cabomaldade.kotlinmasterclass.homefinance
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.cabomaldade.kotlinmasterclass.homefinance.model.Transacao
 import kotlinx.android.synthetic.main.activity_main_card.*
+import java.util.*
 
-//const val TAG = "MainActivity"
+const val TAG = "MainActivity"
 
 class MainActivity: AppCompatActivity()  {
 
@@ -12,7 +16,7 @@ class MainActivity: AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_card)
 
-        valorDaTransacao.setText(0.0.toString())
+        valorDaTransacao.setText(0.0.toString()) // inicia o valor a apresentar na tela
 
         // Atribuindo OnclickListener para todos botões
         somaUm.setOnClickListener{ valorAtual(1.0) }
@@ -25,13 +29,34 @@ class MainActivity: AppCompatActivity()  {
         somaDezCent.setOnClickListener{ valorAtual(0.10) }
         somaVinteCincoCent.setOnClickListener{ valorAtual(0.25) }
         somaCinquentaCent.setOnClickListener{ valorAtual(0.50) }
+
+        btnAdicionar.setOnClickListener{ insereTransacao(criaTransacao())}
     }
 
-    // Atualiza valor da transação na tela  ! falta tratar casas decimais
+    // Atualiza valor da transação na tela  ! Tratado precisão 2 casas decimais
     private fun valorAtual(valorSomar: Double){
         var valor = valorDaTransacao.text.toString().toDouble()
+
         valor += valorSomar
-        valorDaTransacao.setText(valor.toString())
+
+        val somaTotal = String.format(Locale.US,"%.2f", valor)
+
+        valorDaTransacao.setText(somaTotal)
     }
 
+    private fun insereTransacao(transacao: Transacao){
+        Log.d(TAG, "Dados da transação = ${transacao.descricao}, " +
+                "${transacao.valor}, ${transacao.icone}, ${transacao.tipoTransacao}")
+    }
+
+    private fun criaTransacao(): Transacao{
+        //Log.d(TAG, "cria Transacao foi chamada")
+        val tipo = if (transacaoDeEntrada.isChecked) 1 else 2
+        return Transacao(
+            valorDaTransacao.text.toString().toDouble(),
+            "Implementar Spinner",
+            0,
+            tipo
+        )
+    }
 }
